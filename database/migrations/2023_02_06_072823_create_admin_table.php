e434bfc9a75c07744a75dc07a6bbe879f5ef95bf
Aung Myo Kyaw <aungmyokyaw28637@gmail.com>
2023-10-09 14:07:11 +0630

    fix: admin migration error

--- database/migrations/2023_02_06_072823_create_admin_table.php
+++ database/migrations/2023_02_06_072823_create_admin_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('role_id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
